8102dc836d78061ece92ad11111232e0926149e3
Inoki <veyx.shaw@gmail.com>
2024-03-06 22:46:20 +0100

    Add auth class and task caches

--- mstodo/mstodo.py
+++ mstodo/mstodo.py
@@ -1,5 +1,18 @@
+import msal
+
+from mstodo import constant
 
 class MSToDo:
+    def __init__(self, app_name=constant.APP_TITLE, app_version=constant.APP_VERSION):
+        # Set up msal application for this session
+        self._token_cache = msal.SerializableTokenCache()
+        self._app = msal.PublicClientApplication(
+            client_id=constant.MS_AZURE_CLIENT_ID,
+            token_cache=self._token_cache,
+            app_name=app_name,
+            app_version=app_version,
+        )
 
-    def __init__(self):
-        pass
+        # Setup caches for tasks
+        self._task_folders_cache = []
+        self._tasks_cache = []
