d2ed11eae99771aede759ce55f70c3827d89e2b2
Inoki <veyx.shaw@gmail.com>
2024-04-09 22:34:33 +0200

    Add more endpoints to get token and redirect to kodos app

--- examples/webserver/kodos.py
+++ examples/webserver/kodos.py
@@ -2,7 +2,7 @@
 Kindle can show Microsoft To Do
 """
 import base64
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import io
 import qrcode
 import msal
@@ -14,6 +14,7 @@ app = Flask(__name__)
 ms_app = msal.PublicClientApplication(client_id=constant.MS_AZURE_CLIENT_ID, app_name="InokiToDo")
 
 temp_token_mappings = {}
+temp_res_mappings = {}
 
 @app.route("/")
 def index():
@@ -29,10 +30,12 @@ def index():
     buffer = io.BytesIO()
     auth_code_image.save(buffer)
     base64_image = base64.b64encode(buffer.getvalue()).decode()
+    continue_url = f'{request.host_url}app?state={auth_code_flow["state"]}'
     return '<div style="text-align: center; width: 100%;">' +\
         f'<img src="data:image/png;base64,{base64_image}" alt="Login QR code"></p>' +\
         '<p>请扫码登录您的 Microsoft™ 账号</p>' +\
-        f'<p>您的设备拥有完整版浏览器？请点击<a href="{url}">这里</a></p>'
+        f'<p>登陆完成后点击这里<a href="{continue_url}">继续</a>' +\
+        f'<br/><p>您的电纸书设备拥有完整版浏览器？请点击<a href="{url}">这里</a></p>'
 
 
 @app.route("/auth_callback")
@@ -40,15 +43,35 @@ def auth():
     state = request.args.get('state', '')
     if state not in temp_token_mappings:
         # Error
-        return "Failed"
+        return f'<p>登陆失败</p><p>请<a href="{request.host_url}">重试</a></p>'
 
-    auth_token_flow = temp_token_mappings[state]
+    auth_token_flow = temp_token_mappings.pop(state)
     auth_token_reply = request.args.copy()
     res = ms_app.acquire_token_by_auth_code_flow(
         auth_token_flow, auth_token_reply,
         data={"client_secret": CLIENT_SERCERT},
     )
-    return f"Auth callback to get the token: {res}"
+    # Kindle needs to get it later
+    temp_res_mappings.update({state: res})
+
+    continue_url = f"{request.host_url}app?state={state}"
+    return '<div style="text-align: center; width: 100%;">' +\
+        '<p>登录成功</p>' +\
+        '<p>请在电纸书设备上点击“继续”</p>' +\
+        f'<p>或在当前设备点击<a href="{continue_url}">这里</a></p>'
+
+
+@app.route("/auth_token")
+def auth_token():
+    state = request.args.get('state', '')
+    if state not in temp_res_mappings:
+        # Error
+        return jsonify({}), 404, {"Content-Type":"application/json"}
+
+    # Pop: all data will be maintained on the device
+    res = temp_res_mappings.pop(state)
+
+    return jsonify(res), 200, {"Content-Type":"application/json"}
 
 
 @app.route("/auth_refresh")
@@ -59,6 +82,12 @@ def auth_refresh():
         data={"client_secret": CLIENT_SERCERT},
     )
     return f"Refresh auth to get the token: {res}"
+
+
+@app.route("/app")
+def kodos_app():
+    # TODO: Make the page
+    return f"{request.args.get('state', '')}"
 
 
 @app.route("/tasks")
