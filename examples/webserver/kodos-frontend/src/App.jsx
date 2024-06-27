43ea6810d38489da7459760581b7769093f120c4
Inoki <veyx.shaw@gmail.com>
2024-06-27 14:27:09 +0200

    Complete login in kodos

--- examples/webserver/kodos-frontend/src/App.jsx
+++ examples/webserver/kodos-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 import Kodos from './Kodos'
@@ -17,62 +17,83 @@ function getParameterByName(name, url = window.location.href) {
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-function loadUser(state) {
-  console.log(state)
-  fetch(baseURL + '/auth_token?state=' + state,
-    {
-      mode: 'no-cors',
-      method: 'GET'
-    }
-  ).then(res => {
-    console.log("Test")
-    console.log(res)
-    res.json()
-      .then(authInfo => setAuthInfo(authInfo))
-      .catch(error => {console.error(error)})
-  }).catch(error => {
-    console.error("Failed to get info " + error)
-  })
+function parseAndSetAuthInfo(setter, authInfo) {
+  setter(authInfo)
+  login(authInfo)
 }
-
-setTimeout(() => loadUser(getParameterByName("state")), 1000)
 
 function App() {
   const [count, setCount] = useState(0)
   const [isApp, goApp] = useState(false)
-  const [authInfo, setAuthInfo] = useState()
+  const [authInfo, setAuthInfo] = useState(null)
+  const [isLoading, setLoading] = useState(true)
 
   const goBackToApp = () => {
     goApp(true)
   }
+  
+  function loadUser(state) {
+    fetch(baseURL + '/auth_token?state=' + state).then(res => {
+      if (res.status == 200) {
+        res.json()
+          .then(authInfo => parseAndSetAuthInfo(setAuthInfo, authInfo))
+          .catch(error => {
+            console.error(error)
+          })
+      }
+    }).catch(error => {
+      console.error("Failed to get info " + error)
+    })
+  }
 
+  useEffect(() => {
+    if (!authInfo) {
+      setLoading(true)
+      // TODO: Load from local storage
+      const state = getParameterByName("state")
+      if (state) {
+        loadUser(state)
+      }
+      setLoading(false)
+    }
+  })
   
   return (
-    <>
-      <div className="card">
-        {
-          authInfo ?
-          <>
-            <UserContext.Provider value={authInfo.id_token_claims}>
-              {
-                isApp ?
-                  <Kodos count={count} /> :
-                  <KodosSettings filters={[{name: "date", description: "截止日期", availableOptions: ["true", "false"]}, {name: "like", description: "收藏", availableOptions: ["true", "false"]}]} selectedFilter={"date"} back={goBackToApp} />
-              }
-            </UserContext.Provider>
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-            <button onClick={ () => goApp(!isApp) }>
-              Siwtch
-            </button>
-          </> :
-          <>
-            载入账号信息中...
-          </>
-        }
-      </div>
-    </>
+    <div className="card">
+      {
+        authInfo ?
+        <>
+          <UserContext.Provider value={authInfo.id_token_claims}>
+            {
+              isApp ?
+                <Kodos count={count} /> :
+                <KodosSettings filters={[{name: "date", description: "截止日期", availableOptions: ["true", "false"]}, {name: "like", description: "收藏", availableOptions: ["true", "false"]}]} selectedFilter={"date"} back={goBackToApp} />
+            }
+          </UserContext.Provider>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={ () => goApp(!isApp) }>
+            Siwtch
+          </button>
+        </> :
+        <>
+          <div>
+          {
+            isLoading ?
+              <p>
+              载入账号信息中...
+              </p>
+              :
+              <p>
+                您尚未登录 Micrsoft 账号
+                请点击这里<a href="/">登录</a>
+              </p>
+          }
+          </div>
+        </>
+      }
+    </div>
   )
 }
 
